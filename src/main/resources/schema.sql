df36c52b448def931a8343b28d009c01e7242511
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-07-26 01:00:49 +0900

    build: JPA validate로 ddl-auto 변경

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS USERFILE
-(
-    id bigint auto_increment not null,
-    user_id varchar(255) not null,
-    file_path varchar(255) not null unique,
-    file_type varchar(10),
-    primary key (id)
-);
